8eff468725254c3cdbddbacc44cfb2c1fab1e0d7
Martha <martasobotyk@gmail.com>
2023-11-29 15:03:27 +0200

    add: new functionality

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React,{useState} from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import './index.css';
+import App from './App-v2';
+import StarRating from './StarRating';
+import './index'
+
+function Test(){
+  const [movieRating, setMovieRating] = useState(0);
+  return (<>
+  <StarRating color="blue" maxRating={7} onSetRating={setMovieRating}/>
+  <p>This movies was rated {movieRating} stars</p>
+  </>)
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    {/* <StarRating maxRating={5} messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}/>
+    <StarRating maxRating={7} size={30} color="red" className="test" defaultRating={3}/> */}
+    {/* <Test /> */}
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
